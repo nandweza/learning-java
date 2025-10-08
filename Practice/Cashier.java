9888a5436429b11a64ab9dce8d8a29f8e7945a8d
nandweza <allannandweza@gmail.com>
2025-10-07 23:39:15 -0600

    fixed code to handle multiple bills

--- Practice/Cashier.java
+++ Practice/Cashier.java
@@ -2,10 +2,12 @@
 // A simple cashier program that calculates total cost with tax and handles payment.
 
 import java.util.Scanner;
+import java.text.DecimalFormat;
 
 public class Cashier {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        DecimalFormat df = new DecimalFormat("0.00");
 
         double price = 0.0;
 
@@ -19,27 +21,35 @@ public class Cashier {
             total += price;
         } while (price != 0.0);
 
-        System.out.println("Your total amount before tax is: " + total);
+        System.out.println("Your total amount before tax is: " + df.format(total));
 
         double totalCost = (total * TAX_RATE) + total;
-        System.out.println("Your total amount after tax is: " + totalCost);
+        System.out.println("Your total amount after tax is: " + df.format(totalCost));
 
         System.out.print("Please enter your bill amount: ");
         bill = scan.nextDouble();
-        
-        if (bill < total) {
-            double remainingBalance = totalCost - bill;
-            System.out.print("You still have a balance of: " + remainingBalance + 
-                            "\n Please enter another bill: ");
-            double bill2 = scan.nextDouble();
-            change = bill2 - remainingBalance;
-            System.out.println("Your change is: " + change + " Payment received!");
-        } else {
-            System.out.println("Payment received!");
 
-            change = bill - totalCost;
-            System.out.println("Your change is: " + change);
+        while (bill < total) {
+            System.out.print("Enter another bill amount: ");
+            bill += scan.nextDouble();
         }
+
+        change = bill - totalCost;
+        System.out.println("Your change is: " + df.format(change));
+        
+        // if (bill < total) {
+        //     double remainingBalance = totalCost - bill;
+        //     System.out.print("You still have a balance of: " + df.format(remainingBalance) + 
+        //                     "\n Please enter another bill: ");
+        //     double bill2 = scan.nextDouble();
+        //     change = bill2 - remainingBalance;
+        //     System.out.println("Your change is: " + df.format(change) + " Payment received!");
+        // } else {
+        //     System.out.println("Payment received!");
+
+        //     change = bill - totalCost;
+        //     System.out.println("Your change is: " + df.format(change));
+        // }
         scan.close();
     }
 }
